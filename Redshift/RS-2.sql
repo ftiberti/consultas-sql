f0c3f67a7f4787c78c8b7783abe6796e2873c5d0
Franco Tiberti <franco.tiberti87@gmail.com>
2023-09-11 18:24:13 -0300

    cambio

--- Redshift/RS-2.sql
+++ Redshift/RS-2.sql
@@ -14,4 +14,11 @@ where store_id = 767
 select *
 from tiendanube.mwp_orders mo 
 
+--------
 
+select sum(ptf.price)
+FROM tiendanube.mwp_payment_transaction_fees ptf
+LEFT JOIN tiendanube.mwp_payments mp ON ptf.payment_id = mp.id
+where mp.state = 2 
+and cast(mp.paid_at as date) between '2023-08-01' and '2023-08-31'
+and mp.currency = 'BRL'
